e410d318bb6fc4df917cce3006555757f7a0130f
Alessandro <alessandro.parrinoap@gmail.com>
2020-03-03 15:31:11 +0100

    v1

---
+++ cmake-build-debug/CMakeFiles/Labo2_ASD.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Alessandro/Desktop/ProjetsINF2/Labo2_ASD/main.cpp" "C:/Users/Alessandro/Desktop/ProjetsINF2/Labo2_ASD/cmake-build-debug/CMakeFiles/Labo2_ASD.dir/main.cpp.obj"
+  "C:/Users/Alessandro/Desktop/ProjetsINF2/Labo2_ASD/pieces.cpp" "C:/Users/Alessandro/Desktop/ProjetsINF2/Labo2_ASD/cmake-build-debug/CMakeFiles/Labo2_ASD.dir/pieces.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Labo2_ASD.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labo2_ASD.dir/main.cpp.obj"
+  "CMakeFiles/Labo2_ASD.dir/pieces.cpp.obj"
+  "Labo2_ASD.exe"
+  "Labo2_ASD.exe.manifest"
+  "Labo2_ASD.pdb"
+  "libLabo2_ASD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labo2_ASD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
